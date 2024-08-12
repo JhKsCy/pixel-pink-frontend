b563d6d339509958aabda88741399059332d7230
Juliana <juospina@unal.edu.co>
2024-08-12 18:58:48 -0500

    first commit

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guard/auth.guard';
+
+
+export const routes: Routes = [
+    { path: '', title: `Home`, component: HomeComponent  },
+    { path: 'register', title: `Register`, component: RegisterComponent  },
+    { path: 'login', title: `Login`, component: LoginComponent }
+];
