66f2209d2eb0e972bed346f94f1fee8f1e3a30ab
LinaPeña <lina.sierra@grupor5.com>
2024-08-28 19:53:41 -0500

    actualizacion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,8 +9,6 @@ import { CategoriasComponent } from './components/categorias/categorias.componen
 import { PurchaseEmailComponent } from './components/purchase-email/purchase-email.component';
 import { PersonalDataComponent } from './components/personal-data/personal-data.component';
 import { PaymentComponent } from './components/payment/payment.component';
-
-
 import { NewDirectionComponent } from './components/new-direction/new-direction.component';
 import { DefaultDataComponent } from './components/default-data/default-data.component';
 
@@ -26,10 +24,8 @@ export const routes: Routes = [
     { path: 'purchase', title: `Purchase`, component: PurchaseEmailComponent},
     { path: 'personalData', title: `PeronalData`, component: PersonalDataComponent},
     { path: 'payment', title: `Payment`, component: PaymentComponent},
-
     { path: 'collection/:clotheCollection', title: `Collection`, component: CollectionComponent },
     { path: 'product/:id', title: `Product`, component: ProductComponent},
-
     { path: 'newDirection', title: `NewDirection`, component: NewDirectionComponent},
     { path: 'default', title: `Default`, component: DefaultDataComponent}
 ];
